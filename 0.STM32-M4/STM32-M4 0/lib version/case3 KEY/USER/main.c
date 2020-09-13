6c84138210f5da75e0202f83d45811d8dc8d15db
GKCB <hmkscbo@sina.com>
2020-09-13 20:38:28 +0800

    clear project

--- 0.STM32-M4/STM32-M4 0/lib version/case3 KEY/USER/main.c
+++ 0.STM32-M4/STM32-M4 0/lib version/case3 KEY/USER/main.c
@@ -15,6 +15,30 @@ int main(void)
 	LED0 = 0;
 	while(1)
 	{
+		key = KEY_Scan(0); //get value
+		if(key)
+		{
+			switch(key)
+			{
+				case WKUP_PRES: //control BEEP
+					BEEP = !BEEP; //reverse value				
+					break;
+				case KEY0_PRES: //
+					LED0 = !LED0;
+					break;
+				case KEY1_PRES: //
+					LED1 = !LED1;
+					break;
+				case KEY2_PRES: //
+					LED0 = !LED0;
+				  LED1 = !LED1;
+					break;
+			}
+		}
+		else
+		{
+			delay_ms(10);
+		}
 	}
 		
 }
